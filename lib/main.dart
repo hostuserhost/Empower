d8738c91685ca2b6fed88a7a8547364a6beaaabf
host <workhostwork@proton.me>
2023-09-01 17:33:18 +0300

    make quotes parsing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:motivation_alarm/pages/home.dart';
 
 void main() async {
@@ -10,16 +10,16 @@ void main() async {
 
   runApp(
     EasyLocalization(
-        supportedLocales: [ /*Locale('en', 'US'), Locale('ru', 'RU') */],
-        path: 'assets/translations',
-        fallbackLocale: Locale('en', 'US'),
-        child: const Initapp(),
+      supportedLocales: const [Locale('en', 'US')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en', 'US'),
+      child: const Initapp(),
     ),
   );
 }
 
 class Initapp extends StatefulWidget {
-  const Initapp({ super.key });
+  const Initapp({super.key});
 
   @override
   State<Initapp> createState() => _InitappState();
@@ -28,15 +28,29 @@ class Initapp extends StatefulWidget {
 class _InitappState extends State<Initapp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      home: DynamicColorBuilder(
-          builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-            return home();
-          }
-      ),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(428, 926),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) {
+          return DynamicColorBuilder(
+              builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+            return MaterialApp(
+              theme: ThemeData(
+                colorScheme: lightDynamic,
+                useMaterial3: true,
+              ),
+              darkTheme: ThemeData(
+                colorScheme: darkDynamic,
+                useMaterial3: true,
+              ),
+              themeMode: ThemeMode.dark,
+              localizationsDelegates: context.localizationDelegates,
+              supportedLocales: context.supportedLocales,
+              locale: context.locale,
+              home: const Home(),
+            );
+          });
+        });
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,19 +1,71 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:dio/dio.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class home extends StatelessWidget{
+class Home extends StatelessWidget {
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
+    Color primcolor = Colors.blue.harmonizeWith(const ColorScheme.highContrastDark().primary);
     return Scaffold(
-      body: Column(
+        body: SafeArea(
+      child: Column(
         children: [
-          Align(child: AutoSizeText('JUST DO IT'), alignment: Alignment.topLeft,),
-          Container(height: 10,),
-          Container(color: Colors.blue,)
-
+          const Align(
+            alignment: Alignment(-0.7, 0.6),
+            child: AutoSizeText(
+              'JUST DO IT',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            height: 14.h,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                color: primcolor,
+                border: Border.all(
+                  color: primcolor,
+                ),
+                borderRadius: const BorderRadius.all(Radius.circular(16)).w),
+            height: 7.h,
+            width: 383.w,
+          ),
+          Container(
+            height: 14.h,
+          ),
+          Container(
+            padding:  const EdgeInsets.all(5).w,
+            height: 166.h,
+            width: 383.w,
+            decoration: BoxDecoration(
+                border: Border.all(color: primcolor),
+                color: primcolor,
+                borderRadius: const BorderRadius.all(Radius.circular(16)).w),
+            child: FutureBuilder(
+                future: getquotes(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<Response<dynamic>> snapshot) {
+                  return AutoSizeText(
+                    jsonDecode(snapshot.data.toString())['quote'],
+                    style: const TextStyle(fontSize: 30),
+                  );
+                }),
+          ),
         ],
       ),
-    );
+    ));
   }
+}
 
+Future<Response> getquotes() async {
+  final quot = await Dio().get(
+    'https://quote-generator-api-six.vercel.app/api/quotes/random',
+  );
+  return quot;
 }
